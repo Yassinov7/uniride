5b3f2b9d96cbf1c8f21d15631071f114ca0589bc
Mohammad-Yassine-eng <2002m.yassine@gmail.com>
2025-07-09 20:36:19 +0300

    loading timeout

--- src/store/loadingStore.js
+++ src/store/loadingStore.js
@@ -1,6 +1,27 @@
 import { create } from 'zustand';
+import toast from 'react-hot-toast';
 
-export const useLoadingStore = create((set) => ({
-  isLoading: false,
-  setLoading: (val) => set({ isLoading: val }),
-}));
+export const useLoadingStore = create((set) => {
+  let timeoutId = null;
+
+  return {
+    isLoading: false,
+    setLoading: (val) => {
+      if (val) {
+        if (timeoutId) clearTimeout(timeoutId);
+
+        // تايمر تلقائي بعد 10 ثواني
+        timeoutId = setTimeout(() => {
+          set({ isLoading: false });
+          timeoutId = null;
+          toast.error('انتهى الوقت المخصص للتحميل. تأكد من الاتصال.');
+        }, 12000);
+      } else {
+        if (timeoutId) clearTimeout(timeoutId);
+        timeoutId = null;
+      }
+
+      set({ isLoading: val });
+    },
+  };
+});
